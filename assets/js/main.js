a4c5e703e1460f4ea3c64e02b71e00a2a6ee7e0c
Miray <cinziapansini1973@gmail.com>
2020-07-01 02:09:45 +0200

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ function download(filename, text) {
 
 document.getElementById("dwn-btn").addEventListener("click", function(){
 
-    var text = '/**\n* @name BetterUserBackgrounds\n* @version 1.0\n* @description Custom Modal and Pop Up User Backgrounds!\n* @source https://github.com/MirayXS/BetterUserBackgrounds\n* @website https://mirayxs.github.io/BetterUserBackgrounds\n* @authorId 325605285731500033\n* @author XxMirayxX21#3561\n**/\n@import url("https://mirayxs.github.io/BetterUserBackgrounds/assets/css/base.css");\n[user_by_bdfdb*="'+document.getElementById("text-val").value+'"],\n[data-user-id*="'+document.getElementById("text-val").value+'"] {\n	--user-background: url('+document.getElementById("text-val2").value+');}'
+    var text = '/**\n* @name BetterUserBackgrounds\n* @version 1.0\n* @description Custom Modal and Pop Up User Backgrounds!\n* @source https://github.com/MirayXS/BetterUserBackgrounds\n* @website https://mirayxs.github.io/BetterUserBackgrounds\n* @authorId 325605285731500033\n* @author XxMirayxX21#3561\n**/\n@import url("https://discord-custom-covers.github.io/usrbg/snippets/modals.css");\n@import url("https://discord-custom-covers.github.io/usrbg/snippets/userPopouts.css");\n[user_by_bdfdb*="'+document.getElementById("text-val").value+'"],\n[data-user-id*="'+document.getElementById("text-val").value+'"] {\n	--user-background: url('+document.getElementById("text-val2").value+');\n}'
     var filename = "BetterUserBackgrounds.theme.css";
     
     download(filename, text);
